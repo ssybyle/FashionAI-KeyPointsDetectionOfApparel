5c6c15fb61b1f0dc7d389ab8184ea33b2a51e602
Sibylle <sibylle.azeau@grenoble-inp.org>
2025-02-12 19:28:01 +0100

    correction

--- src/stage2/predict_one.py
+++ src/stage2/predict_one.py
@@ -127,6 +127,7 @@ if __name__ == '__main__':
         if args.vis:
             kp_img = draw_keypoints(img0, keypoints)
             cv2.imwrite(config.proj_path + '/tmp/{0}{1}.png'.format(config.clothes, idx), kp_img)
+            cv2.imshow(f"Keypoints for {idx}", kp_img)
 
         left, right = config.datum
         x1, y1, v1 = kpts[left]
