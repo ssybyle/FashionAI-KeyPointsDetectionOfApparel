c19e38314c2ab8832e6e86aeb1f07e39235df5db
Sibylle <sibylle.azeau@grenoble-inp.org>
2025-03-07 12:32:39 +0100

    remove file

--- src/stage2/detect.py
+++ src/stage2/detect.py
@@ -113,7 +113,7 @@ if __name__ == '__main__':
         keypoints = np.stack([x, y, np.ones(x.shape)], axis=1).astype(np.int16)
 
         #CREATION FICHIER TEXTE
-        if (os.exists('/content/'+img_file+'.txt')):
+        if (os.path.exists('/content/'+img_file+'.txt')):
             os.remove('/content/'+img_file+'.txt')
         file = open('/content/'+img_file+'.txt', "x")
         file.write(str(keypoints))
